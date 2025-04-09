cdb8305f6ba4838a32e8ac5fd6a3fb0e3f74c4c5
unknown <gleb2010krasavchik@icloud.com>
2025-04-09 16:49:40 +0300

    Add all

---
+++ 1.04/DataBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace Employee_DataBase;
+
+
+public class DataBase
+{
+    public static void ReadInfo(Employee employee)
+    {
+        Console.WriteLine($"Age:{employee.Age} | " + $"Name: {employee.Name} | " + $"Surname: {employee.Surname} | " +
+                          $"Position: {employee.Position}");
+    }
+
+    public static Employee AddEmployee()
+    {
+        int age = 0;
+        bool IsWorking1 = true;
+        do
+        {
+            try
+            {
+                Console.Write("What`s your age: ");
+                int age1 = int.Parse(Console.ReadLine());
+                age = age1;
+                IsWorking1 = false;
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input || Try again");
+            }
+        } while (IsWorking1 != false);
+
+        Console.Write("What`s your name: ");
+        string name = Console.ReadLine();
+        Console.Write("What`s your surname: ");
+        string surname = Console.ReadLine();
+        Console.Write("What`s your position: ");
+        string position = Console.ReadLine();
+        
+        Employee employee = new Employee(age, position, surname, name);
+        return employee;
+    }
+
+    public static void ChangeInfo(List<Employee> dt, int number)
+    {
+        int age = 0;
+        bool IsWorking1 = true;
+        do
+        {
+            try
+            {
+                Console.Write("What`s your age: ");
+                int age1 = int.Parse(Console.ReadLine());
+                age = age1;
+                IsWorking1 = false;
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input || Try again");
+            }
+        } while (IsWorking1 != false);
+
+        Console.Write("What`s your name: ");
+        string name = Console.ReadLine();
+        Console.Write("What`s your surname: ");
+        string surname = Console.ReadLine();
+        Console.Write("What`s your position: ");
+        string position = Console.ReadLine();
+
+        dt[number].Name = name;
+        dt[number].Position = position;
+        dt[number].Age = age;
+        dt[number].Surname = surname;
+    }
+
+    public static void ClearInfo(List<Employee> dt)
+    {
+        dt.Clear();
+    }
+    public static void ClearEmployeeInfo(int index , List<Employee> dt)
+    {
+        dt.RemoveAt(index);
+    }
+}
---
+++ 1.04/Program.cs
@@ -0,0 +1,72 @@
+﻿
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_DataBase
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+        do
+        {
+            
+            List<Employee> Db = new List<Employee>();
+            Db.Add(DataBase.AddEmployee());
+               Selector:
+                Console.WriteLine();
+                Console.WriteLine("Possible actions ( type number ) : ");
+                Console.WriteLine("1 - Exit");
+                Console.WriteLine("2 - Add new employee");
+                Console.WriteLine("3 - Read information");
+                Console.WriteLine("4 - Change information");
+                Console.WriteLine("5 - Delete all information");
+                Console.WriteLine("6 - Delete an employee information");
+                Console.Write(">");
+                var answer = Console.ReadLine();
+                
+                switch (answer)
+                {
+                    case "1":
+                        Console.WriteLine("Goodbye! || See you soon");
+                        Environment.Exit(0);
+                        break;
+                    case "2":
+                        Db.Add(DataBase.AddEmployee());
+                        Console.Clear();
+                        goto Selector;
+                     case "3":
+                        for (int i = 0; i < Db.Count; i++)
+                        {
+                            Console.Write(i+". ");
+                            DataBase.ReadInfo(Db[i]);
+                        }
+                        Console.WriteLine();
+                        Thread.Sleep(2100);
+                        goto Selector;
+                     case "4" :
+                            Console.WriteLine("Employee number in the list: ");
+                            int EmpNumber = int.Parse(Console.ReadLine());
+                            DataBase.ChangeInfo(Db, EmpNumber);
+                            Console.Clear();        // Hello
+                        goto Selector;
+                         
+                     case "5":
+                         DataBase.ClearInfo(Db);
+                         Console.Clear();
+                         goto Selector;
+                     case "6":
+                         Console.WriteLine();
+                         Console.WriteLine("Employee number in the list: ");
+                         int EmpIndex = int.Parse(Console.ReadLine());
+                         DataBase.ClearEmployeeInfo(EmpIndex , Db);
+                         Console.Clear();
+                         goto Selector;
+                }
+                
+            } while (true);
+            
+        }
+    }
+}
